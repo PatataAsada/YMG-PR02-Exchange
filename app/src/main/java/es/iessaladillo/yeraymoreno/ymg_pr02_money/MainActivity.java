fd945bca216114d3d40a86ebaed46921ba7b5c7e
Yeray <yeray3000@gmail.com>
2018-09-30 12:26:20 +0200

    Starting to work in the button, and changing some things

--- app/src/main/java/es/iessaladillo/yeraymoreno/ymg_pr02_money/MainActivity.java
+++ app/src/main/java/es/iessaladillo/yeraymoreno/ymg_pr02_money/MainActivity.java
@@ -2,7 +2,6 @@ package es.iessaladillo.yeraymoreno.ymg_pr02_money;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
